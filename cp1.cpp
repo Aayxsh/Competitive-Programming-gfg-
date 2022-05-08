3c6c7823b102f05d4c6076b096c5b8a1765c88e1
Aayush Anshul <99768325+Aayxsh@users.noreply.github.com>
2022-05-08 22:07:15 +0530

    Update cp1.cpp

--- cp1.cpp
+++ cp1.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int arr[6]={1,2,3,4,5,6};
 
-void relocate_print(int in, int array[6], int nu){
+void relocate_print(int in, int array[6], int lmao){
 	for(int u=6; u>in;u=u-1){
 	 array[u]=array[u-1];
 	 }
 
-	array[in]=nu;
+	array[in]=lmao;
 
 	for(int i=0; i<=6; i++){
 	 cout << array[i]<<"\n";
